69ff46146a0e1db70652c25161742c59d9a12902
unknown <shikharsingh267@gmail.com>
2022-08-03 08:07:45 -0700

    added voting smart contract

---
+++ Voting/contracts/Voter.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.0;
+pragma experimental ABIEncoderV2;
+
+//["coffee","tea"]
+// contract Voter {
+    uint[] public votes;
+    string[] public  options;
+    constructor(string[] _options) public {
+        options = _options;
+        votes.length = options.length;
+    }
+    function vote(uint option) public {
+        require(0 <= option && option < options.length, "Invalid Option");
+        votes[option] = votes[option] + 1;
+    }
+    function getOptions() public view returns (string[]){
+        return options;
+    }
+
+    function getVotes() public view returns (uint[]){
+        return votes;
+    }
+}
